32349a009dc07e899996834a97fd1e9e2dc2b0b2
depose <philippxbeer@gmail.com>
2025-04-09 23:28:40 +0200

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,12 +1,8 @@
-import asyncio
 from sources.twitter import scrape_twitter
+import asyncio
 
 async def main():
     await scrape_twitter()
-    # Later: add other sources like Discord or Dune here
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
-
-
